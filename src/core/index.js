63de4ec3dd1eca6b14b7a59eb00285c80d0a059e
aksenov <aksenov@gnivc.ru>
2023-06-18 00:27:17 +0300

    начал создавать структуру

--- src/core/index.js
+++ src/core/index.js
@@ -1,35 +1,50 @@
 'use strict';
 
 // include libraries
+require('../libraries/common/index');
 require('../libraries/cryptopro/index');
 require('../libraries/rutoken/index');
 
 // main class
-function WebSign() {
-	
-	// certificate list. public property
-  this.certificates = [];
+class WebSign // extends EventTarget
+{
 
-	// library events. private property
-  var libraryEvents = [];
+	// certificate list.
+	#certificates = new Map();
 
-	// library instaces. private property
-  var libraryInstances = [];
+	// library instaces.
+	#libraryInstances = new Set();
 
-	// closure - pointer to current instance of class. private property
-  var self = this;
-  
-  // class constructor code
-  
-  // register libraries
-  libraryInstances.push(new WebSignCryptoPro());
-  libraryInstances.push(new WebSignRutoken());
+	certificateAddedCallbacks = new Set();
+	certificateRemovedCallbacks = new Set();
+	errorCallbacks = new Set();
+	signCallbacks = new Set();
 
-	// private function
-  function onReady() {
-  }
+	constructor()
+	{
+		// register libraries
+		var cp = new WebSignCryptoPro();
+		cp.certificateAddedCallbacks.add(function (cert)
+		{
+			this.#certificates.add(cert.id, cert);
+			this.certificateAddedCallbacks.forEach(function (v) { v(cert); });
+		});
+		this.#libraryInstances.push(cp);
 
-	// public function
-  this.run = function() {
-  };
+		var rt = new WebSignRutoken();
+		rt.certificateAddedCallbacks.add(function (cert)
+		{
+			this.#certificates.delete(cert.id);
+			this.certificateAddedCallbacks.forEach(function (v) { v(cert); });
+		});
+		this.#libraryInstances.push(rt);
+	}
+
+	SignHash(certificateId, hashAsHex)
+	{
+		// detect library
+		// call library
+		// save result
+		this.signCallbacks.forEach(function (v) { v("Signature"); });
+	}
 }
