5854672451c1457291dd3407224b146a65642ef7
aksenov <aksenov@gnivc.ru>
2023-06-20 01:23:08 +0300

    попробовал документацию tsdoc

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,53 +1,136 @@
-// include libraries
 import { webSignCertificate, webSignError, webSignSignature, webSignInterface } from '../libraries/common/';
 import webSignCryptoPro from '../libraries/cryptopro/';
 import webSignRutoken from '../libraries/rutoken/';
 
-// main class
+/** Main web sign class */
 export default class webSign
 {
-	// certificate list.
+	/** Internal list of available certificates */
 	private certificateList = new Map<string, webSignCertificate>();
 
-	// library instaces.
+	/** Internal list of modules */
 	private libraryInstances = new Set<webSignInterface>();
 
-	public onCertificateAdd?: (certificate:webSignCertificate) => void;
-	public onCertificateRemove?: (certificate: webSignCertificate) => void;
-	public onError?: (error: webSignError) => void;
-	public onSignComplete?: (error: webSignSignature) => void;
+	/** 
+		*  Event. New certificate is available
+		* @remarks 
+		* This event occurs when modules found new available certificate for sign.
+	  * Works after call to startCertificateScan.
+		*
+		* @param certificate Information about new certificate
+	*/
+	public onCertificateAdd?: (certificate: webSignCertificate) => void;
 
+	/**
+		*  Event. Certificate is now unavailable.
+		*
+		* @remarks
+		* This event occurs when modules detect certificate removal.
+	  * Works after call to startCertificateScan.
+		*
+		* @param certificate Information about removed certificate
+	*/
+	public onCertificateRemove?: (certificate: webSignCertificate) => void;
+
+	/**
+		*  Event. Common error in any function.
+		*
+		* @remarks
+		* This event occurs when any error occurs.
+		*
+		* @param certificate Information about error
+	*/
+	public onError?: (error: webSignError) => void;
+
+	/**
+		*  Event. Sign is completed.
+		*
+		* @remarks
+		* This event occurs when sign hash completes and produce signature.
+		*
+		* @param certificate Information about signature
+	*/
+	public onSignComplete?: (signature: webSignSignature) => void;
+
+	/** Constructor creates objects for all modules and subscribe for all its events. */
 	constructor()
 	{
-		// register libraries
+		// register CryptoPro
 		let cp = new webSignCryptoPro();
 		cp.onCertificateAdd = (certificate: webSignCertificate) =>
 		{
 			this.certificateList.set(certificate.id, certificate);
 			if (this.onCertificateAdd) this.onCertificateAdd(certificate);
 		}
+		cp.onCertificateRemove = (certificate: webSignCertificate) =>
+		{
+			this.certificateList.delete(certificate.id);
+			if (this.onCertificateRemove) this.onCertificateRemove(certificate);
+		}
+		cp.onError = (error: webSignError) =>
+		{
+			if (this.onError) this.onError(error);
+		}
+		cp.onSignComplete = (signature: webSignSignature) =>
+		{
+			if (this.onSignComplete) this.onSignComplete(signature);
+		}
 		this.libraryInstances.add(cp);
 
+		// register Rutoken
 		let rt = new webSignRutoken();
 		rt.onCertificateAdd = (certificate: webSignCertificate) =>
 		{
 			this.certificateList.set(certificate.id, certificate);
 			if (this.onCertificateAdd) this.onCertificateAdd(certificate);
 		}
+		rt.onCertificateRemove = (certificate: webSignCertificate) =>
+		{
+			this.certificateList.delete(certificate.id);
+			if (this.onCertificateRemove) this.onCertificateRemove(certificate);
+		}
+		rt.onError = (error: webSignError) =>
+		{
+			if (this.onError) this.onError(error);
+		}
+		rt.onSignComplete = (signature: webSignSignature) =>
+		{
+			if (this.onSignComplete) this.onSignComplete(signature);
+		}
 		this.libraryInstances.add(rt);
 	}
 
-	public startCertificateScan()
+	/** Start searching for available certificates. */
+	public startCertificateScan():void
 	{
+		// subsequently start searching in all modules
 		this.libraryInstances.forEach((ws) => ws.startCertificateScan());
 	}
 
-	public stopCertificateScan()
+	/** Stop searching for available certificates. */
+	public stopCertificateScan(): void
 	{
+		// subsequently stop searching in all modules
 		this.libraryInstances.forEach((ws) => ws.stopCertificateScan());
 	}
 
-	public signHash(certificate: webSignCertificate, algorithmOid: string, hashAsHex: string)
+	/**
+		* Sign hash with certificate
+		*
+		* @remarks
+		* Sign hash using provided certificate and hash algorithm
+		*
+		* @param certificate Certificate to sign
+		* @param algorithmOid Object identifier of hashing algorithm
+		* @example
+		* 1.2.643.7.1.1.2.2 - GOST R 34.11-12 with length 256
+		* 1.2.643.7.1.1.2.3 - GOST R 34.11-12 with length 512
+	  * 2.16.840.1.101.3.4.2.1 - Secure Hash Algorithm that uses a 256 bit key (SHA256)
+		* @param hashAsHex Hex string of bytes of hash value
+		* @example
+		* for oid 1.2.643.7.1.1.2.2 - 00557be5e584fd52a449b16b0251d05d27f94ab76cbaa6da890b59d8ef1e159d
+		*/
+	public signHash(certificate: webSignCertificate, algorithmOid: string, hashAsHex: string): void
 	{
 		// save result
 		if (this.onSignComplete) this.onSignComplete(new webSignSignature(certificate, algorithmOid, hashAsHex, "Signature core"));
