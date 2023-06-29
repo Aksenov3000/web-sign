f4d3cd0b2e5d9a593c0dfe69a15d4738a44394e1
aksenov <aksenov@gnivc.ru>
2023-06-29 16:33:07 +0300

    разделил конфиг дева и прода для vite

--- wbmes/src/vite.config.ts
+++ wbmes/src/vite.config.ts
@@ -1,25 +1,42 @@
 import { defineConfig } from "vite";
 import dts from 'vite-plugin-dts';
 
-/** @type {import('vite').UserConfig} */
-export default defineConfig({
-    server:
-    {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true
-    },
-    build:
-    {
-        lib:
-        {
-            entry: 'src/core/index.ts',
-            name: 'web-sign',
-            formats: ['cjs'],
-            fileName: (format) => `index.js`
-        },
-        outDir: 'build',
-        emptyOutDir: true,
-    },
-    plugins: [dts({ outputDir: 'bbb' })]
+
+export default defineConfig(({ command, mode }) =>
+{
+	console.log("defineConfig - " + command);
+
+	if (command === 'serve')
+	{
+		// command === 'serve'
+		return {
+			server:
+			{
+				host: '0.0.0.0',
+				port: 5173,
+				strictPort: true
+			}
+		}
+	}
+	else
+	{
+		// command === 'build'
+		return {
+			build:
+			{
+				minify: true,
+				reportCompressedSize: true,
+				lib:
+				{
+					entry: ['src/core/index.ts'],
+					name: 'web-sign',
+					formats: ['cjs'],
+					fileName: (format) => `index.js`
+				},
+				outDir: 'build',
+				emptyOutDir: true,
+			},
+			plugins: [dts()]
+		}
+	}
 });
